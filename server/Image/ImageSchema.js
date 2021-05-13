6367c3b1dfc605fec0ba35fd167469c82c9a4502
Robert, Drew <ddrobe2@ilstu.edu>
2021-05-12 21:16:39 -0500

    added some additional fields

--- server/Image/ImageSchema.js
+++ server/Image/ImageSchema.js
@@ -4,5 +4,6 @@ export const ImageSchema = new mongoose.Schema(
 	{
 		url: String,
 		source: String,
+		sourceId: String
 	}
 );
--- server/PostFeed/PostFeedSchema.js
+++ server/PostFeed/PostFeedSchema.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 export const PostFeedSchema = new mongoose.Schema(
 	{
 		active: Boolean,
+		name: String,
+		description: String,
 		adminUserIds: [String]
 	},
 	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
